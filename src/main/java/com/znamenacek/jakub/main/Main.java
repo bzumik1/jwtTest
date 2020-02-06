f87601371e62af4d91fa9daa3310c3bcef398857
Jakub Znamenáček <bzumik1@users.noreply.github.com>
2020-02-06 10:06:09 +0100

    asymetric

--- src/main/java/com/znamenacek/jakub/main/Main.java
+++ src/main/java/com/znamenacek/jakub/main/Main.java
@@ -1,5 +1,6 @@
 package com.znamenacek.jakub.main;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -11,11 +12,15 @@ import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.interfaces.DSAPrivateKey;
+import java.sql.Date;
 import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
     //secret key algorithms
+        System.out.println("SECRET KEY ALGORITHMS");
         Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512); //generate key
         String store = Encoders.BASE64.encode(key.getEncoded()); // this can be stored
         byte[] load = Decoders.BASE64.decode(store); // this is how to load stored key
@@ -31,9 +36,28 @@ public class Main {
 
 
     //asymmetric key algorithms
+        System.out.println("ASYMETRIC KEY ALGORITHMS:");
         KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.ES256);
         PublicKey publicKey = keyPair.getPublic();
         PrivateKey privateKey = keyPair.getPrivate();
+
+        var student = new Student("Jakub","Znameanacek");
+
+        String jwsAsymetric = Jwts.builder()
+                .setSubject("Jakub")
+                .claim("student",student)
+                .setIssuedAt(Date.valueOf(LocalDate.now().minusDays(2)))
+                .setExpiration(Date.valueOf(LocalDate.now().plusDays(1)))
+                .signWith(privateKey)
+                .compact();
+
+        System.out.println(jwsAsymetric);
+        //Decode
+        Claims body = Jwts.parser().setSigningKey(publicKey).parseClaimsJws(jwsAsymetric).getBody();
+//        HashMap<String, Long> hashMap = (HashMap<String, Long>)body.get("student");
+//        System.out.println(hashMap.get("birthday"));
+        Student decodedStudent = body.get("student",Student.class); //Doesn't work
+        System.out.println(decodedStudent);
 
 
 
---
+++ src/main/java/com/znamenacek/jakub/main/Student.java
@@ -0,0 +1,11 @@
+package com.znamenacek.jakub.main;
+
+import lombok.*;
+
+import java.util.Date;
+
+@AllArgsConstructor @Setter @Getter @ToString @NoArgsConstructor
+public class Student {
+    String name;
+    String surname;
+}
